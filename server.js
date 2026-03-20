94b14ac12f6d24cd01a87983c0b27a54740253a3
DavidSW123 <ds.qnk.88@gmail.com>
2026-03-20 16:23:30 +0100

    Cambios rutas de localhost para la nube

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({limit: '10mb'})); 
+app.use(express.static(__dirname)); // <--- AÑADE ESTA LÍNEA PARA QUE LEA LAS FOTOS Y LA APP
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/bbdd', (req, res) => res.sendFile(path.join(__dirname, 'bbdd.html')));
